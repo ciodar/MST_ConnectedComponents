3a816b6e32fcaeef6bc60bd6653e9fed8b383d14
xioni3 <42072580+xioni3@users.noreply.github.com>
2019-12-22 17:55:12 +0100

    Implemented MST and visualizer

--- Grafo.py
+++ Grafo.py
@@ -9,6 +9,7 @@ class Grafo:
 
     def __init__(self, n, p):
         self.nodi,self.m = self.createRandom(n, p)
+        self.parent = dict()
     #def __init__(self):
     #    self.nodi = set()
     #    self.m = set()
@@ -20,14 +21,14 @@ class Grafo:
         for i in range(n):
             nodo = Nodo(i)
             nodi.add(nodo)
+            if i not in m.keys():
+                m[i] = set()
             for j in range(n):
                 r = random.random()
                 if r <= float(p)/100:
                     w = random.randint(WEIGHT_MIN, WEIGHT_MAX)
                     edge = (j,w)
                     edge2 = (i,w)
-                    if i not in m.keys():
-                        m[i] = set()
                     m[i].add(edge)
                     if j not in m.keys():
                         m[j] = set()
@@ -40,11 +41,38 @@ class Grafo:
         return self.find(parent, parent[i])
 
         #ordina in modo decrescente i nodi in base al loro tempo di completamento di visita
-    def sort(self):
+    def node_sort(self):
         self.nodi.sort(key=lambda x: x.f, reverse=True)
 
-    def archi_sort(self):
-        self.m = sorted(self.m, key=lambda item: item[2])
+    def edge_sort(self):
+        edges = set()
+        for k, v in grafo.m.items():
+            for vv in v:
+                edges.add((k, vv[0],vv[1]))
+        edges = sorted(edges, key=lambda item: item[2])
+        return edges
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of
+        # high rank tree (Union by Rank)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+
+            # If ranks are same, then make one as root
+        # and increment its rank by one
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
 
     def get_all_connected_groups(self):
         already_seen = set()
@@ -61,28 +89,107 @@ class Grafo:
         while nodes:
             node = nodes.pop()
             already_seen.add(node)
-            nodes = nodes or {v[0] for v in grafo.m[node]} - already_seen
+            for n in {v[0] for v in grafo.m[node]} - already_seen - nodes:
+                nodes.add(n)
             result.append(node)
         return result, already_seen
+    def draw(self):
+        G = nx.Graph()
+        G.add_nodes_from(map(lambda Nodo: Nodo.id, grafo.nodi))
+        for k, v in grafo.m.items():
+            for vv in v:
+                G.add_edge(k, vv[0], weight=vv[1])
+        esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= 3]
+        enormal = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 3 and d['weight'] <= 5]
+        elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 5 and d['weight'] <= 7]
+        exlarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 7]
+        # nx.draw(G, with_labels=True, font_weight='bold')
+        pos = nx.circular_layout(G)  # positions for all nodes
+        # nodes
+        nx.draw_networkx_nodes(G, pos, node_size=500)
+        # edges
+        nx.draw_networkx_edges(G, pos, edgelist=esmall, width=1, edge_color='b', style='dashed')
+        nx.draw_networkx_edges(G, pos, edgelist=enormal, width=1, edge_color='g')
+        nx.draw_networkx_edges(G, pos, edgelist=elarge, width=1, edge_color='y')
+        nx.draw_networkx_edges(G, pos, edgelist=exlarge, width=1, edge_color='r')
+        # labels
+        nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
+        plt.show()
 
+    def KruskalMST(self):
 
+        result = []  # This will store the resultant MST
+
+        i = 0  # An index variable, used for sorted edges
+        e = 0  # An index variable, used for result[]
+
+        # Step 1:  Sort all the edges in non-decreasing
+        # order of their
+        # weight.  If we are not allowed to change the
+        # given graph, we can create a copy of graph
+        edges = self.edge_sort()
+
+        parent = []
+        rank = []
+
+        # Create V subsets with single elements
+        for node in range(len(self.nodi)):
+            parent.append(node)
+            rank.append(0)
+        if len(edges) < len(self.nodi) - 1:
+            print("Given graph can't be connected")
+            return
+            # Number of edges to be taken is equal to V-1
+        while e < len(self.nodi) - 1:
+
+            # Step 2: Pick the smallest edge and increment
+            # the index for next iteration
+            u, v, weight = edges[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            # If including this edge does't cause cycle,
+            # include it in result and increment the index
+            # of result for next edge
+            if x != y:
+                e = e + 1
+                result.append([u, v, weight])
+                self.union(parent, rank, x, y)
+                # Else discard the edge
+        H = nx.Graph()
+        H.add_nodes_from(map(lambda Nodo: Nodo.id, grafo.nodi))
+        # print the contents of result[] to display the built MST
+        print("Following are the edges in the constructed MST")
+        for u, v, weight in result:
+
+            H.add_edge(u,v,weight=weight)
+            # print str(u) + " -- " + str(v) + " == " + str(weight)
+            print("%d -- %d == %d" % (u, v, weight))
+        esmall = [(u, v) for (u, v, d) in H.edges(data=True) if d['weight'] <= 3]
+        enormal = [(u, v) for (u, v, d) in H.edges(data=True) if d['weight'] > 3 and d['weight'] <= 5]
+        elarge = [(u, v) for (u, v, d) in H.edges(data=True) if d['weight'] > 5 and d['weight'] <= 7]
+        exlarge = [(u, v) for (u, v, d) in H.edges(data=True) if d['weight'] > 7]
+        # nx.draw(G, with_labels=True, font_weight='bold')
+        pos = nx.circular_layout(H)  # positions for all nodes
+        # nodes
+        nx.draw_networkx_nodes(H, pos, node_size=500)
+        # edges
+        nx.draw_networkx_edges(H, pos, edgelist=esmall, width=1, edge_color='b', style='dashed')
+        nx.draw_networkx_edges(H, pos, edgelist=enormal, width=1, edge_color='g')
+        nx.draw_networkx_edges(H, pos, edgelist=elarge, width=1, edge_color='y')
+        nx.draw_networkx_edges(H, pos, edgelist=exlarge, width=1, edge_color='r')
+        # labels
+        nx.draw_networkx_labels(H, pos, font_size=10, font_family='sans-serif')
+        plt.show()
 if __name__ == '__main__':
     print("creating new grafo")
-    G = nx.Graph()
-    grafo = Grafo(20,5)
-    G.add_nodes_from(map(lambda Nodo: Nodo.id, grafo.nodi))
-    for k, v in grafo.m.items():
-        for vv in v:
-            G.add_edge(k,vv[0])
-    #grafo.nodi = (0, 1, 2, 3, 4)
-    #edge = (1, 0, 1)
-    #grafo.m.add(edge)
-    #edge = (2, 3, 1)
-    #grafo.m.add(edge)
-    #edge = (3, 4, 1)
-    #grafo.m.add(edge)
+
+    grafo = Grafo(20,10)
+    grafo.draw()
+
     cc = grafo.get_all_connected_groups()
-    for nodo in cc:
-        print(nodo)
-    nx.draw(G, with_labels=True, font_weight='bold')
-    plt.show()
+    print(cc)
+    edges = grafo.edge_sort()
+    print(edges)
+    grafo.KruskalMST()
